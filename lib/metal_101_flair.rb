5ce99bd3148cc38d2cfb9d69da6b338afe53509a
Andy Buntine <bunts@hhd.com.au>
2013-08-03 11:17:04 +1000

    Started fleshing out structure

---
+++ lib/metal_101_flair.rb
@@ -0,0 +1,15 @@
+require "net/http"
+require "json"
+
+# Get 100 threads, order by new
+# See if new comments exist in each
+  # Delete those that have not changed
+  # Update database with new comments count
+
+# Check comments for each
+  # Delete comments older than 6 hours
+  # Search for magic words
+    # If found, find parent and record
+
+# Collate into comment
+# PM mods
