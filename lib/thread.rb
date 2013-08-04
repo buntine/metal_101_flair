6d298b8a455017caa8863d71e7d19d49c560f0b9
Andy Buntine <bunts@hhd.com.au>
2013-08-04 11:12:57 +1000

    Continued

--- lib/thread.rb
+++ lib/thread.rb
@@ -5,12 +5,28 @@ class Thread
   def initialize(name, author, num_comments, permalink)
     @name = name
     @author = author
-    @num_comments = num_comments
+    @num_comments = num_comments.to_i
     @permalink = permalink
+    @cache = "./dat/threads.yml"
   end
 
   def should_check?
-    # Check for any comments
+    @num_comments > 0 and previous_num_comments < @num_comments
+  end
+
+  def cache_comment_count!
+    threads = YAML.load(File.open(@cache))
+    threads[@name] = @num_comments
+    File.open(@cache, "w") do |f|
+      f.write(threads.to_yaml)
+    end
+  end
+
+ private
+
+  def previous_num_comments
+    threads = YAML.load(File.open(@cache))
+    threads[@name]
   end
 
 end
